255e58030ae3ca91fe61778d61778f792203d5b3
Qingkai Kong <kongqk@berkeley.edu>
2014-02-19 22:50:25 -0800

    init mongoDB repo

---
+++ script/fruit_form.py
@@ -0,0 +1,62 @@
+import bottle
+
+@bottle.route('/')
+def home_page():
+    mythings = ['apple', 'orange', 'banana', 'peach']
+    return bottle.template('hello_world2', username='Qingkai', things=mythings)
+#    return bottle.template('hello_world',{'username':"Fan", 'things':mythings})
+
+@bottle.post('/favorite_fruit')
+def favorite_fruit():
+    fruit = bottle.request.forms.get('fruit')
+    if (fruit == None or fruit == ""):
+        fruit = "No Fruit Selected"
+        
+    bottle.response.set_cookie("fruit",fruit)
+    bottle.redirect("/show_fruit")   
+    #return bottle.template('fruit_selections.tpl', {'fruit':fruit})
+    
+@bottle.route('/show_fruit')
+def show_fruit():
+    fruit = bottle.request.get_cookie("fruit")
+    
+    return bottle.template('fruit_selections.tpl', {'fruit':fruit})    
+
+bottle.debug(True)
+bottle.run(host='localhost', port=8080)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
